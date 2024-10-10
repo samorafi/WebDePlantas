4fc6d274b92b6c0820889be5820e222537b317f4
samorafi <sebasmora172522@gmail.com>
2024-10-10 17:58:47 -0600

    Initial commit

---
+++ src/main/java/com/WebDePlantas/WebDePlantasApplication.java
@@ -0,0 +1,13 @@
+package com.WebDePlantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDePlantasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDePlantasApplication.class, args);
+	}
+
+}
